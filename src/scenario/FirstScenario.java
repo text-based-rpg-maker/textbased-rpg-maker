4d09015122671bf1f752882202c19311434cce06
João Paulo Ferreira <iamferreirajp@gmail.com>
2017-06-19 15:14:32 -0300

    Create Scenario State Creating Scenario, ScenarioState, ScenarioInterface and FirstScenario to implement other scenarios later.
    Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com>

---
+++ src/scenario/FirstScenario.java
@@ -0,0 +1,20 @@
+package scenario;
+
+public class FirstScenario implements ScenarioState{
+	
+	public ScenarioState goNorth(){
+		return null;
+	}
+	
+	public ScenarioState goSouth(){
+		return null;
+	}
+	
+	public ScenarioState goWest(){
+		return null;
+	}
+	
+	public ScenarioState goEast(){
+		return null;
+	}
+}
---
+++ src/scenario/ScenarioInterface.java
@@ -0,0 +1,8 @@
+package scenario;
+
+public interface ScenarioInterface {
+	public void goNorth();
+	public void goSouth();
+	public void goWest();
+	public void goEast();
+}
