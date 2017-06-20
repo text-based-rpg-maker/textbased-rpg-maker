0379535aa35c62b30c8f565f5a6f0fcfefe9667e
João Paulo Ferreira <iamferreirajp@gmail.com>
2017-06-19 22:24:02 -0300

    Modificando chamada dos métodos
    Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com>

--- src/scenario/DefaultScenario.java
+++ src/scenario/DefaultScenario.java
@@ -1,8 +1,53 @@
 package scenario;
 
-public class DefaultScenario extends ScenarioState{
+import java.util.Scanner;
 
-	
+public class DefaultScenario implements ScenarioState{
+
+	public DefaultScenario(){
+		
+		//Descrição do cenário
+		System.out.println("Nome: Cenário Inicial");
+		System.out.println("Descrição: Descrição qualquer!");
+		System.out.println("Escolha alguma opção: ");
+		System.out.println("a) Ir para o norte.");
+		System.out.println("b) Ir para o sul.");
+		System.out.println("c) Ir para o oeste.");
+		System.out.println("d) Ir para o leste.");
+		System.out.println("e) Pegar item.");
+		System.out.println("f) Usar item.");
+		
+		while(true){
+			@SuppressWarnings("resource")
+			Scanner keyboard = new Scanner(System.in); 
+			String choice = keyboard.nextLine();
+			
+			//Seletor da ação
+			switch(choice) {
+				case "a":
+					this.goNorth();
+					break;
+				case "b":
+					this.goSouth();
+					break;
+				case "c":
+					this.goWest();
+					break;
+				case "d":
+					this.goEast();
+					break;
+				case "e":
+					this.getItem();
+					break;
+				case "f":
+					this.useItem();
+					break;
+				default:
+					System.out.println("Escolha uma opção válida");
+					break;
+			}
+		}
+	}
 	@Override
 	public ScenarioState goNorth(){
 		System.out.println("Não há caminho para o norte");
