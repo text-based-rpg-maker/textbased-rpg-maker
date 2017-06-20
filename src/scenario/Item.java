8394c277e5c64ca345f654e8cc7f3b453427e981
Marcelo Martins <martins.oliveira.mo@gmail.com>
2017-06-20 20:59:00 -0300

    Criando classes Item e Inventario

---
+++ src/scenario/Item.java
@@ -0,0 +1,28 @@
+package scenario;
+
+public class Item {
+	
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private String description;
+	
+}
