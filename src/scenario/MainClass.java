3a6700ad47c3199846703f4d1dbbd4b90bf2e77e
Antonio Carvalho <_antonio_@hotmail.com>
2017-06-21 01:21:05 -0300

    Adicionando chamada para coleção de itens

---
+++ src/scenario/MainClass.java
@@ -0,0 +1,17 @@
+package scenario;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		Item item = new Item();
+		item.setDescription("dasdsa");
+		item.setId(22);
+		item.setName("sads");
+		
+		ItemCollection.registerItem(item);
+		
+		Scenario scenario = new Scenario(new DefaultScenario(item));
+		
+	}
+
+}
