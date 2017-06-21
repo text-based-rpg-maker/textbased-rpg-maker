82ba0470b5e61ddc45d20478d48292cdceeb59c1
Flavio Costa <skingk.f@gmail.com>
2017-06-21 03:07:59 -0300

    Adicionando método move e refatorando a classe
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- src/scenario/SecondScenario.java
+++ src/scenario/SecondScenario.java
@@ -6,16 +6,17 @@ public class SecondScenario implements ScenarioState{
 	
 	private Item item;
 
+	private final int UNLOCK_ITEM_ID = 22;
+	private ScenarioState nextStep;
+	
 	public SecondScenario(){
 		
-		behavior();
 	}
 	
 	public SecondScenario(Item item){
 		
 		this.item = item;
 		
-		behavior();
 	}
 	
 	public void behavior(){
@@ -25,27 +26,15 @@ public class SecondScenario implements ScenarioState{
 	}
 	
 	@Override
-	public ScenarioState goNorth(){
-		System.out.println("Não há caminho para o norte");
-		return this;
-	}
-	
-	@Override
-	public ScenarioState goSouth(){
-		System.out.println("Se movendo para o sul!");
-		return new DefaultScenario();
-	}
-	
-	@Override
-	public ScenarioState goWest(){
-		System.out.println("Não há caminho para o oeste");
-		return this;
-	}
-	
-	@Override
-	public ScenarioState goEast(){
-		System.out.println("Não há caminho para o leste");
-		return this;
+	public ScenarioState move(ScenarioState state){
+		if (state.isLocked() && !Inventory.getItens().contains(state.unlockingItem())){
+			System.out.println("O caminho está bloqueado!");
+			nextStep = this;
+		} else {
+			System.out.println("Indo para outro cenário!");
+			nextStep = state;
+		}
+		return null;
 	}
 
 	@Override
@@ -56,11 +45,6 @@ public class SecondScenario implements ScenarioState{
 		}else{
 			this.item = null;
 		}
-		return this;
-	}
-	@Override
-	public ScenarioState useItem() {
-		System.out.println("Não houve nenhum efeito");
 		return this;
 	}
 
@@ -79,18 +63,13 @@ public class SecondScenario implements ScenarioState{
 		
 		if(item != null){
 			System.out.println(item.getDescription());
-			
+
 		}		
 	}
 	
 	@Override
-	public boolean isLocked(){
-		return true;
-	}
-	
-	@Override
-	public int unlockingItem(){
-		return 22;
+	public Item unlockingItem(){
+		return ItemCollection.getItemById(UNLOCK_ITEM_ID);
 	}
 
 	@Override
@@ -106,30 +85,25 @@ public class SecondScenario implements ScenarioState{
 			System.out.println("c) Ir para o oeste.");
 			System.out.println("d) Ir para o leste.");
 			System.out.println("e) Pegar item.");
-			System.out.println("f) Usar item.");
 			
 			choice = keyboard.nextLine();
 			
 			switch(choice) {
 			case "a":
-				this.goNorth();
+				this.move(this);
 				break;
 			case "b":
-				this.goSouth();
+				this.move(new DefaultScenario());
 				break;
 			case "c":
-				this.goWest();
+				this.move(this);
 				break;
 			case "d":
-				this.goEast();
+				this.move(this);
 				break;
 			case "e":
 				exit = false;
 				this.getItem();
-				break;
-			case "f":
-				exit = false;
-				this.useItem();
 				break;
 			default:
 				System.out.println("Escolha uma opção válida");
@@ -140,12 +114,22 @@ public class SecondScenario implements ScenarioState{
 		
 		} while(!exit);
 		
-		keyboard.close();
 	}
 
 	@Override
 	public void handleOption() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public ScenarioState getNextStep() {
+		return nextStep;
+	}
+
+	@Override
+	public boolean isLocked() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 }
