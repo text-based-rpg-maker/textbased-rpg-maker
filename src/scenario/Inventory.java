ea4a051da87f97ec0d591d62047e7e475ff87810
Marcelo Martins <martins.oliveira.mo@gmail.com>
2017-06-20 22:04:56 -0300

    Tornando metodos static e criação do get and set

--- src/scenario/Inventory.java
+++ src/scenario/Inventory.java
@@ -6,20 +6,26 @@ import java.util.ArrayList;
 
 public class Inventory {
 	
-	private List<Item> itens = new ArrayList<Item>();
+	private static List<Item> itens = new ArrayList<Item>();
 	
-	public void addItemToInventory(Item item){
+	public static void addItemToInventory(Item item){
 		
 		itens.add(item);
-		System.out.println("Item adicionado com sucesso");
 	}
 	
-	public void  showItemIventory(){
+	public static void showItemIventory(){
 		
 		for(Item item : itens){
 			System.out.println(item.getName());
 		}
 	}
-	
 
+	public static List<Item> getItens() {
+		return itens;
+	}
+
+	public static void setItens(List<Item> itens) {
+		Inventory.itens = itens;
+	}
+	
 }
--- src/scenario/Item.java
+++ src/scenario/Item.java
@@ -4,6 +4,7 @@ public class Item {
 	
 	private int id;
 	private String name;
+	private String description;
 	
 	public int getId() {
 		return id;
@@ -23,6 +24,5 @@ public class Item {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	private String description;
 	
 }
