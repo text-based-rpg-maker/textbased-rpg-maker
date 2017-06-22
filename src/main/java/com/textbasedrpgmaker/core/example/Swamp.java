dae6ba9e6ba98caee30bf6c73b9816fb1b8a5fe4
=Marcelo Martins <martins.oliveira.mo@gmail.com>
2017-06-22 20:40:15 -0300

    Removendo dependencia do scenarioState da classe Swamp

--- src/main/java/com/textbasedrpgmaker/core/example/Swamp.java
+++ src/main/java/com/textbasedrpgmaker/core/example/Swamp.java
@@ -2,7 +2,6 @@ package com.textbasedrpgmaker.core.example;
 
 import com.textbasedrpgmaker.core.item.Item;
 import com.textbasedrpgmaker.core.scenario.Location;
-import com.textbasedrpgmaker.core.scenario.ScenarioState;
 
 public class Swamp extends Location {
 
@@ -14,7 +13,6 @@ public class Swamp extends Location {
 		super();
 	}
 	
-	@Override
 	public void showDescription() {
 		System.out.println("Pântano da Perdição");
 		System.out.println("O pântano diante de você se estende por infindaveis"
@@ -22,54 +20,45 @@ public class Swamp extends Location {
 				+ "\nÁrvores retorcidas, musgos e animais rastejantes dominam o ambiente.");
 	}
 
-	@Override
-	public ScenarioState northLocation() {
+	public Location northLocation() {
 		return new Castle();
 	}
 
-	@Override
-	public ScenarioState southLocation() {
+	public Location southLocation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public ScenarioState eastLocation() {
+	public Location eastLocation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public ScenarioState westLocation() {
+	public Location westLocation() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String optionA() {
 		// TODO Auto-generated method stub
 		return "Ir em direção ao castelo";
 	}
 
-	@Override
 	public String optionB() {
 		// TODO Auto-generated method stub
 		return "Vagar no pântano para sempre";
 	}
 
-	@Override
 	public String optionC() {
 		// TODO Auto-generated method stub
 		return "Vagar no pântano para sempre";
 	}
 
-	@Override
 	public String optionD() {
 		// TODO Auto-generated method stub
 		return "Vagar no pântano para sempre";
 	}
 
-	@Override
 	public String optionItem() {
 		// TODO Auto-generated method stub
 		return "Pegar chave do castelo";
