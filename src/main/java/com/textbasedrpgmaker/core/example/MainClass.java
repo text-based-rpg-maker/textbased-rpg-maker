a89694b9efd0f26d1682e683941d82b1e7db5468
Adailson Santos <adailson2@gmail.com>
2017-06-22 15:09:02 -0300

    Retirado item quando estiver coletado

--- src/main/java/com/textbasedrpgmaker/core/example/MainClass.java
+++ src/main/java/com/textbasedrpgmaker/core/example/MainClass.java
@@ -6,7 +6,7 @@ import com.textbasedrpgmaker.core.scenario.Scenario;
 import com.textbasedrpgmaker.core.startgame.GamePlay;
 
 public class MainClass {
-	
+
 	public static void main(String[] args) {
 		Item chave = new Item(1, "Chave do castelo", "Uma chave velha para o castelo.");
 		ItemCollection.registerItem(chave);
