b4f660f36a72280ff910c21552b718b78536971d
Adailson Santos <adailson2@gmail.com>
2017-06-22 13:22:28 -0300

    Resolvendo loop da classe Scenario
    Signed-off-by: Adailson Santos <adailson2@gmail.com>

---
+++ src/main/java/com/textbasedrpgmaker/core/startgame/GamePlay.java
@@ -0,0 +1,31 @@
+package com.textbasedrpgmaker.core.startgame;
+
+import com.textbasedrpgmaker.core.scenario.Scenario;
+
+public class GamePlay {
+	
+	private Scenario scenario;
+	private static boolean endGame;
+	public GamePlay(){
+		endGame = false;
+	}
+	public static boolean isEndGame() {
+		return endGame;
+	}
+	public static void setEndGame(boolean endGame) {
+		GamePlay.endGame = endGame;
+	}
+	public Scenario getScenario() {
+		return scenario;
+	}
+	public void setScenario(Scenario scenario) {
+		this.scenario = scenario;
+	}
+	public void startGame(){
+		while(!endGame){
+			scenario.behavior();
+			scenario.changeState();
+		}
+	}
+	
+}
