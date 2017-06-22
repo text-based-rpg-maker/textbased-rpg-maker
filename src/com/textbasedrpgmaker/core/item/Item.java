0d9b3df895ea8eb4b235787ebecdb305c7c31082
Lucas Amoêdo <lucas_advc@outlook.com>
2017-06-22 10:42:44 -0300

    Cria novo construtor para Item

--- src/com/textbasedrpgmaker/core/item/Item.java
+++ src/com/textbasedrpgmaker/core/item/Item.java
@@ -6,23 +6,37 @@ public class Item {
 	private String name;
 	private String description;
 	
-	public int getId() {
-		return id;
+	public Item() {
+		
 	}
-	public void setId(int id) {
+	
+	public Item(int id, String name, String description) {
 		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
 		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
 		this.description = description;
 	}
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
